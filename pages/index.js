8271a638986ff10db93edb2c10de225d862658f6
Fkysino <92769291+Fkysino@users.noreply.github.com>
2022-07-14 22:58:00 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Love me!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
